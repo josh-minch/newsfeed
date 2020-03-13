8d152127f3475844dc6c128371497eeac1156a8f
Josh Minch <josh.minch@gmail.com>
2020-03-12 22:49:21 -0700

    Remove old saved headlines, null model constraint

--- feed/models.py
+++ feed/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Article(models.Model):
     category = models.CharField(max_length=200)
-    title = models.CharField(max_length=200, null=True)
-    source = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200)
+    source = models.CharField(max_length=200)
     description = models.TextField(null=True)
     url = models.URLField(null=True)
     url_to_image = models.URLField(null=True)
