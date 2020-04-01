5f58f1ddb4666636407f5aeeec8569dcb108ec20
Josh Minch <josh.minch@gmail.com>
2020-04-01 15:23:23 -0700

    Update Celery, RabbitMQ settings

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: gunicorn newsfeed.wsgi
-worker: celery -A newsfeed worker -l info
-beat: celery -A newsfeed beat -l info
+worker: celery -A newsfeed worker -B -l info
--- newsfeed/settings.py
+++ newsfeed/settings.py
@@ -139,18 +139,18 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'feed:frontpage'
 
 # Celery settings
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost'
+CELERY_BROKER_URL = get_env_value('CLOUDAMQP_URL')
 
 #: Only add pickle to this list if your broker is secured
 #: from unwanted access (see userguide/security.html)
+CELERY_RESULT_BACKEND = get_env_value('CLOUDAMQP_URL')
 CELERY_ACCEPT_CONTENT = ['json']
-CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
 CELERY_TASK_SERIALIZER = 'json'
 
 CELERY_BEAT_SCHEDULE = {
     'refresh_articles': {
         'task': 'feed.tasks.refresh_articles',
-        'schedule': crontab(minute='*/1'),
+        'schedule': crontab(minute='*/23'),
     }
 }
 
