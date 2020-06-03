f41089a263e211cb47aa836e815f92789251d581
Josh Minch <josh.minch@gmail.com>
2020-06-02 17:35:52 -0700

    Remove commented out code

--- newsfeed/settings.py
+++ newsfeed/settings.py
@@ -86,14 +86,6 @@ WSGI_APPLICATION = 'newsfeed.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'newsfeed',
-    #     'USER': 'postgres',
-    #     'PASSWORD': get_env_value('DB_PASSWORD'),
-    #     'HOST': 'jm-newsfeed.herokuapp.com',
-    #     'PORT': '5432',
-    # },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
